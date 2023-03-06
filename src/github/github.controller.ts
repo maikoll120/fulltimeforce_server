aa12ff9fdcb8e2e39ade94cf588f2388d0a5face
Maikoll Soto <maikoll120@gmail.com>
2023-03-06 14:38:18 -0400

    Create github Module

---
+++ src/github/github.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Get()
+  findAll() {
+    return this.githubService.findAll();
+  }
+}
